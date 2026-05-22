3d5bacda466ec7fddc01bb3fdd30d3257be7a163
unknown <rizkiramdhanrizki36484@gmail.com>
2026-05-23 03:29:14 +0700

    fix: buka akses cors

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,16 @@ import pembicaraRoute from './routes/pembicaraRoute.js';
 const app = express();
 const port = 3000;
 
+// ️ PERBAIKAN: Mengizinkan domain localhost dan domain Vercel Live Anda
 app.use(cors({
-  origin: 'http://localhost:5173' // atau cukup cors() untuk mengizinkan semua
+  origin: [
+    'http://localhost:5173', 
+    'https://utsfrontend-kohl.vercel.app'
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
 }));
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -17,9 +24,8 @@ app.get('/', (req, res) => {
 });
 
 app.use("/events", eventRoutes);
-app.use("/categories", categoryRoutes); // Endpoint: http://localhost:3000/categories
+app.use("/categories", categoryRoutes); 
 app.use("/speakers", pembicaraRoute);
-
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
